670504ec94e071d9b11e39de05e3088c63570682
Liching Li (Admin) <P696328a@aur.national.com.au>
2017-01-30 15:38:42 +1100

    Add how to use program details to the CLI

--- ToyRobot/Program.cs
+++ ToyRobot/Program.cs
@@ -11,12 +11,21 @@ namespace ToyRobot
         {
             RobotParser robotParser = new RobotParser(new Robot());
 
+            Console.WriteLine(  "Toy Robot Simulator. \n" +
+                                "\nRobot commands: " +
+                                "\nPLACE X,Y,F : Place Robot on table" +
+                                "\nMOVE: Move Robot one unit in direction it is facing" + 
+                                "\nLEFT: Rotate Robot left by 90 degrees" + 
+                                "\nRIGHT: Rotate Robot right by 90 degrees" + 
+                                "\nREPORT: Report Robot position" + 
+                                "\nEXIT: Quit simulator\n");
+
             while (true)
             {
                 Console.Write(">");
                 string command = Console.ReadLine();
 
-                if (command == "q")
+                if (command == "EXIT")
                     break;
 
                 if (robotParser.parse(command) == false)
