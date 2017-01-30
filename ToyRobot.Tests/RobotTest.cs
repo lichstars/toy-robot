33ef960c8fb65b75a2d22c2cb3500ed2c6cf06b4
Liching Li (Admin) <P696328a@aur.national.com.au>
2017-01-30 13:38:35 +1100

    Add Robot and RobotParser unit test cases.

---
+++ ToyRobot.Tests/RobotTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ToyRobot.Tests
+{
+    [TestClass]
+    public class RobotTest
+    {
+        [TestMethod]
+        public void Robot_ShouldNotAcceptMoveCommand_WhenNotFirstPlacedOnTable()
+        {
+            Robot robot = new Robot();
+            bool result = robot.send("MOVE");
+            Assert.IsFalse(result);
+        }
+        [TestMethod]
+        public void Robot_ShouldAcceptMoveCommand_AfterPlacedOnTable()
+        {
+            Robot robot = new Robot();
+            robot.send("PLACE 0,0,NORTH");
+            bool result = robot.send("MOVE");
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Robot_ShouldNotAcceptPlaceCommand_WhenLocationIsOffTable()
+        {
+            Robot robot = new Robot();
+            bool result = robot.place("PLACE -1,0,NORTH");
+            Assert.IsFalse(result);
+        }
+    }
+}
