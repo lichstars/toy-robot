7e2ecc7499f023d9d550b1725aaba9c0040a2bf7
lichstars <lichstars@gmail.com>
2017-02-12 15:34:29 +1100

    Adjust code to work with Nunit and Xamarin

--- ToyRobot.Tests/RobotParserTest.cs
+++ ToyRobot.Tests/RobotParserTest.cs
@@ -1,63 +1,59 @@
-﻿using System;
-using System.Text;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using NUnit.Framework;
 
-namespace ToyRobot.Tests
+namespace ToyRobot.RobotParserTest
 {
-    [TestClass]
-    public class RobotParserTest
-    {
-        [TestMethod]
-        public void RobotParser_ShouldIgnoreUnrecognisedCommand()
-        {
-            var parser = new RobotParser(new Robot());
-            var result = parser.parse("HELLO");
-            Assert.IsFalse(result);
-        }
-        [TestMethod]
-        public void RobotParser_ShouldAcceptPlaceCommand()
-        {
-            var parser = new RobotParser(new Robot());
-            var result = parser.parse("PLACE 0,0,NORTH");
-            Assert.IsTrue(result);
-        }
-        [TestMethod]
-        public void RobotParser_ShouldReturnFalseOnPlaceCommand_WhenNotEnoughArgumentsGiven()
-        {
-            var parser = new RobotParser(new Robot());
-            var result = parser.parse("PLACE 1,NORTH");
-            Assert.IsFalse(result);
-        }
-        [TestMethod]
-        public void RobotParser_ShouldReturnFalseOnPlaceCommand_WhenXAxisIsNotNumber()
-        {
-            var parser = new RobotParser(new Robot());
-            var result = parser.parse("PLACE X,1,NORTH");
-            Assert.IsFalse(result);
-        }
-        [TestMethod]
-        public void RobotParser_ShouldReturnFalseOnPlaceCommand_WhenYAxisIsNotNumber()
-        {
-            var parser = new RobotParser(new Robot());
-            var result = parser.parse("PLACE 1,Y,NORTH");
-            Assert.IsFalse(result);
-        }
-        [TestMethod]
-        public void RobotParser_ShouldReturnFalseOnPlaceCommand_WhenDirectionIsInvalid()
-        {
-            var parser = new RobotParser(new Robot());
-            var result = parser.parse("PLACE 1,1,NTH");
-            Assert.IsFalse(result);
-        }
-        [TestMethod]
-        public void RobotParser_ShouldNotAcceptTwoCommandsOnOneLine()
-        {
-            RobotParser parser = new RobotParser(new Robot());
-            parser.parse("PLACE 0,0,NORTH");
-            bool result = parser.parse("MOVE REPORT");
-            Assert.IsFalse(result);
-        }
-    }
+	[TestFixture()]
+	public class RobotParserTest
+	{
+		[Test()]
+		public void RobotParser_ShouldIgnoreUnrecognisedCommand()
+		{
+			var parser = new RobotParser(new Robot());
+			var result = parser.parse("HELLO");
+			Assert.IsFalse(result);
+		}
+		[Test()]
+		public void RobotParser_ShouldAcceptPlaceCommand()
+		{
+			var parser = new RobotParser(new Robot());
+			var result = parser.parse("PLACE 0,0,NORTH");
+			Assert.IsTrue(result);
+		}
+		[Test()]
+		public void RobotParser_ShouldReturnFalseOnPlaceCommand_WhenNotEnoughArgumentsGiven()
+		{
+			var parser = new RobotParser(new Robot());
+			var result = parser.parse("PLACE 1,NORTH");
+			Assert.IsFalse(result);
+		}
+		[Test()]
+		public void RobotParser_ShouldReturnFalseOnPlaceCommand_WhenXAxisIsNotNumber()
+		{
+			var parser = new RobotParser(new Robot());
+			var result = parser.parse("PLACE X,1,NORTH");
+			Assert.IsFalse(result);
+		}
+		[Test()]
+		public void RobotParser_ShouldReturnFalseOnPlaceCommand_WhenYAxisIsNotNumber()
+		{
+			var parser = new RobotParser(new Robot());
+			var result = parser.parse("PLACE 1,Y,NORTH");
+			Assert.IsFalse(result);
+		}
+		[Test()]
+		public void RobotParser_ShouldReturnFalseOnPlaceCommand_WhenDirectionIsInvalid()
+		{
+			var parser = new RobotParser(new Robot());
+			var result = parser.parse("PLACE 1,1,NTH");
+			Assert.IsFalse(result);
+		}
+		[Test()]
+		public void RobotParser_ShouldNotAcceptTwoCommandsOnOneLine()
+		{
+			RobotParser parser = new RobotParser(new Robot());
+			parser.parse("PLACE 0,0,NORTH");
+			bool result = parser.parse("MOVE REPORT");
+			Assert.IsFalse(result);
+		}
+	}
 }
--- ToyRobot.Tests/RobotTest.cs
+++ ToyRobot.Tests/RobotTest.cs
@@ -1,147 +1,142 @@
-﻿using System;
-using System.Text;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.IO;
+﻿using NUnit.Framework;
 
-namespace ToyRobot.Tests
+namespace ToyRobot.RobotTest
 {
-    [TestClass]
-    public class RobotTest
-    {
-        [TestMethod]
-        public void Robot_ShouldNotAcceptMoveCommand_WhenNotFirstPlacedOnTable()
-        {
-            Robot robot = new Robot();
-            robot.send("MOVE");
-            string expected = robot.getError();
-            Assert.AreEqual(expected, "Robot must be placed on the table first.");
-        }
-        [TestMethod]
-        public void Robot_ShouldAcceptMoveCommand_AfterPlacedOnTable()
-        {
-            Robot robot = new Robot();
-            robot.send("PLACE 0,0,NORTH");
-            robot.send("MOVE");
-        }
-        [TestMethod]
-        public void Robot_ShouldNotAcceptPlaceCommand_WhenLocationIsOffTable()
-        {
-            Robot robot = new Robot();
-            robot.send("PLACE -1,0,NORTH");
-            string expected = robot.getError();
-            Assert.AreEqual(expected, "That move will move the Robot off the table. Try again.");
-        }
-        [TestMethod]
-        public void Robot_ShouldReportPositionCorrectly()
-        {
-            Robot robot = new Robot();
-            robot.send("PLACE 0,0,NORTH");
-            string result = robot.report();
-            Assert.AreEqual("0,0,NORTH", result);
-        }
-        [TestMethod]
-        public void Robot_ShouldMoveNorthOneUnit_WhenFacingNorthAndMoved()
-        {
-            Robot robot = new Robot();
-            robot.send("PLACE 0,0,NORTH");
-            robot.send("MOVE");
-            string result = robot.report();
-            Assert.AreEqual("0,1,NORTH", result);
-        }
-        [TestMethod]
-        public void Robot_ShouldMoveSouthOneUnit_WhenFacingSouthAndMoved()
-        {
-            Robot robot = new Robot();
-            robot.send("PLACE 0,2,SOUTH");
-            robot.send("MOVE");
-            string result = robot.report();
-            Assert.AreEqual("0,1,SOUTH", result);
-        }
-        [TestMethod]
-        public void Robot_ShouldMoveEastOneUnit_WhenFacingEastAndMoved()
-        {
-            Robot robot = new Robot();
-            robot.send("PLACE 3,0,EAST");
-            robot.send("MOVE");
-            string result = robot.report();
-            Assert.AreEqual("4,0,EAST", result);
-        }
-        [TestMethod]
-        public void Robot_ShouldMoveWestOneUnit_WhenFacingWestAndMoved()
-        {
-            Robot robot = new Robot();
-            robot.send("PLACE 3,0,WEST");
-            robot.send("MOVE");
-            string result = robot.report();
-            Assert.AreEqual("2,0,WEST", result);
-        }
-        [TestMethod]
-        public void Robot_ShouldFaceSouth_WhenTurnedLeftFromWest()
-        {
-            Robot robot = new Robot();
-            robot.send("PLACE 3,0,WEST");
-            robot.send("LEFT");
-            string result = robot.report();
-            Assert.AreEqual("3,0,SOUTH", result);
-        }
-        [TestMethod]
-        public void Robot_ShouldFaceWest_WhenTurnedLeftFromNorth()
-        {
-            Robot robot = new Robot();
-            robot.send("PLACE 3,0,NORTH");
-            robot.send("LEFT");
-            string result = robot.report();
-            Assert.AreEqual("3,0,WEST", result);
-        }
-        [TestMethod]
-        public void Robot_ShouldFaceWest_WhenTurnedRightFromSouth()
-        {
-            Robot robot = new Robot();
-            robot.send("PLACE 3,0,SOUTH");
-            robot.send("RIGHT");
-            string result = robot.report();
-            Assert.AreEqual("3,0,WEST", result);
-        }
-        [TestMethod]
-        public void Robot_ShouldFaceNorth_WhenTurnedRightFromWest()
-        {
-            Robot robot = new Robot();
-            robot.send("PLACE 3,0,WEST");
-            robot.send("RIGHT");
-            string result = robot.report();
-            Assert.AreEqual("3,0,NORTH", result);
-        }
-        [TestMethod]
-        public void Robot_TestExampleA()
-        {
-            Robot robot = new Robot();
-            robot.send("PLACE 0,0,NORTH");
-            robot.send("MOVE");
-            string result = robot.report();
-            Assert.AreEqual("0,1,NORTH", result);
-        }
-        [TestMethod]
-        public void Robot_TestExampleB()
-        {
-            Robot robot = new Robot();
-            robot.send("PLACE 0,0,NORTH");
-            robot.send("LEFT");
-            string result = robot.report();
-            Assert.AreEqual("0,0,WEST", result);
-        }
-        [TestMethod]
-        public void Robot_TestExampleC()
-        {
-            Robot robot = new Robot();
-            robot.send("PLACE 1,2,EAST");
-            robot.send("MOVE");
-            robot.send("MOVE");
-            robot.send("LEFT");
-            robot.send("MOVE");
-            string result = robot.report();
-            Assert.AreEqual("3,3,NORTH", result);
-        }
-    }
+	[TestFixture()]
+	public class RobotTest
+	{
+		[Test()]
+		public void Robot_ShouldNotAcceptMoveCommand_WhenNotFirstPlacedOnTable()
+		{
+			Robot robot = new Robot();
+			robot.send("MOVE");
+			string expected = robot.getError();
+			Assert.AreEqual(expected, "Robot must be placed on the table first.");
+		}
+		[Test()]
+		public void Robot_ShouldAcceptMoveCommand_AfterPlacedOnTable()
+		{
+			Robot robot = new Robot();
+			robot.send("PLACE 0,0,NORTH");
+			robot.send("MOVE");
+		}
+		[Test()]
+		public void Robot_ShouldNotAcceptPlaceCommand_WhenLocationIsOffTable()
+		{
+			Robot robot = new Robot();
+			robot.send("PLACE -1,0,NORTH");
+			string expected = robot.getError();
+			Assert.AreEqual(expected, "That move will move the Robot off the table. Try again.");
+		}
+		[Test()]
+		public void Robot_ShouldReportPositionCorrectly()
+		{
+			Robot robot = new Robot();
+			robot.send("PLACE 0,0,NORTH");
+			string result = robot.report();
+			Assert.AreEqual("0,0,NORTH", result);
+		}
+		[Test()]
+		public void Robot_ShouldMoveNorthOneUnit_WhenFacingNorthAndMoved()
+		{
+			Robot robot = new Robot();
+			robot.send("PLACE 0,0,NORTH");
+			robot.send("MOVE");
+			string result = robot.report();
+			Assert.AreEqual("0,1,NORTH", result);
+		}
+		[Test()]
+		public void Robot_ShouldMoveSouthOneUnit_WhenFacingSouthAndMoved()
+		{
+			Robot robot = new Robot();
+			robot.send("PLACE 0,2,SOUTH");
+			robot.send("MOVE");
+			string result = robot.report();
+			Assert.AreEqual("0,1,SOUTH", result);
+		}
+		[Test()]
+		public void Robot_ShouldMoveEastOneUnit_WhenFacingEastAndMoved()
+		{
+			Robot robot = new Robot();
+			robot.send("PLACE 3,0,EAST");
+			robot.send("MOVE");
+			string result = robot.report();
+			Assert.AreEqual("4,0,EAST", result);
+		}
+		[Test()]
+		public void Robot_ShouldMoveWestOneUnit_WhenFacingWestAndMoved()
+		{
+			Robot robot = new Robot();
+			robot.send("PLACE 3,0,WEST");
+			robot.send("MOVE");
+			string result = robot.report();
+			Assert.AreEqual("2,0,WEST", result);
+		}
+		[Test()]
+		public void Robot_ShouldFaceSouth_WhenTurnedLeftFromWest()
+		{
+			Robot robot = new Robot();
+			robot.send("PLACE 3,0,WEST");
+			robot.send("LEFT");
+			string result = robot.report();
+			Assert.AreEqual("3,0,SOUTH", result);
+		}
+		[Test()]
+		public void Robot_ShouldFaceWest_WhenTurnedLeftFromNorth()
+		{
+			Robot robot = new Robot();
+			robot.send("PLACE 3,0,NORTH");
+			robot.send("LEFT");
+			string result = robot.report();
+			Assert.AreEqual("3,0,WEST", result);
+		}
+		[Test()]
+		public void Robot_ShouldFaceWest_WhenTurnedRightFromSouth()
+		{
+			Robot robot = new Robot();
+			robot.send("PLACE 3,0,SOUTH");
+			robot.send("RIGHT");
+			string result = robot.report();
+			Assert.AreEqual("3,0,WEST", result);
+		}
+		[Test()]
+		public void Robot_ShouldFaceNorth_WhenTurnedRightFromWest()
+		{
+			Robot robot = new Robot();
+			robot.send("PLACE 3,0,WEST");
+			robot.send("RIGHT");
+			string result = robot.report();
+			Assert.AreEqual("3,0,NORTH", result);
+		}
+		[Test()]
+		public void Robot_TestExampleA()
+		{
+			Robot robot = new Robot();
+			robot.send("PLACE 0,0,NORTH");
+			robot.send("MOVE");
+			string result = robot.report();
+			Assert.AreEqual("0,1,NORTH", result);
+		}
+		[Test()]
+		public void Robot_TestExampleB()
+		{
+			Robot robot = new Robot();
+			robot.send("PLACE 0,0,NORTH");
+			robot.send("LEFT");
+			string result = robot.report();
+			Assert.AreEqual("0,0,WEST", result);
+		}
+		[Test()]
+		public void Robot_TestExampleC()
+		{
+			Robot robot = new Robot();
+			robot.send("PLACE 1,2,EAST");
+			robot.send("MOVE");
+			robot.send("MOVE");
+			robot.send("LEFT");
+			robot.send("MOVE");
+			string result = robot.report();
+			Assert.AreEqual("3,3,NORTH", result);
+		}
+	}
 }
